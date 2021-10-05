78fc38829a95d25b1cd2a5b4c5b2e39cc509f6c2
Septiadi Rahmawan <septiadirahmawan@gmail.com>
2021-10-05 20:45:10 +0700

    backup

--- example/example.php
+++ example/example.php
@@ -2,11 +2,18 @@
 
 require '../vendor/autoload.php';
 
-use Septiadi\Aes;
+use Septiadi\Bantuin\Crypt;
+use Septiadi\Bantuin\JSON;
 
 $secret_key = "rahasia";
 
-echo Aes::encrypt("data", $secret_key);
+echo Crypt::aes_encrypt("data", $secret_key);
 echo "<br><br>";
-echo Aes::decrypt("X+qpswtOhhTS4eAMVg08Sg==.enhoSlJVdXRleVpURHBpZw==", $secret_key);
+echo Crypt::aes_decrypt("X+qpswtOhhTS4eAMVg08Sg==.enhoSlJVdXRleVpURHBpZw==", $secret_key);
+echo "<br><br>";
+echo Crypt::md5($secret_key);
+echo "<br><br>";
+$a = ['a' => 123];
+$b = ['a' => 456];
+echo json_encode(JSON::diff($a, $b));
 ?>
